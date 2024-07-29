2f2fb0d8f6ef07ffb3cf7316b45100eb80cdc6eb
parisi18 <parisi18@unifesp.br>
2024-07-29 10:41:02 -0300

    Exercícios modules

--- rustlings/exercises/10_modules/modules2.rs
+++ rustlings/exercises/10_modules/modules2.rs
@@ -6,6 +6,8 @@ mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
+    pub use crate::delicious_snacks::fruits::PEAR as fruit;
+    pub use crate::delicious_snacks::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &str = "Pear";
@@ -19,6 +21,8 @@ mod delicious_snacks {
 }
 
 fn main() {
+
+
     println!(
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
