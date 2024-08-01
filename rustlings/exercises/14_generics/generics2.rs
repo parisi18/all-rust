cbda2b834d46f694aaaeb58ad96b68496ef80a4b
parisi18 <parisi18@unifesp.br>
2024-08-01 10:50:25 -0300

    Exercícios generics

--- rustlings/exercises/14_generics/generics2.rs
+++ rustlings/exercises/14_generics/generics2.rs
@@ -1,11 +1,11 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
+impl<u32> Wrapper<u32> {
     fn new(value: u32) -> Self {
         Wrapper { value }
     }
